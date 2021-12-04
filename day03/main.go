5f32db4af8a763b188eb4e44524ca622b1afbddf
Benjamin Morrison <ben@gbmor.dev>
2021-12-04 02:00:45 -0500

    day 03

---
+++ day03/main.go
@@ -0,0 +1,152 @@
+package main
+
+import (
+	"bytes"
+	"flag"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+	"time"
+)
+
+var flagTest = flag.Bool("test", false, "use the test data")
+
+var partTwoByteMap = map[bool]map[bool]byte{
+	true: {
+		true:  byte('1'),
+		false: byte('0'),
+	},
+	false: {
+		true:  byte('0'),
+		false: byte('1'),
+	},
+}
+
+func getReport(test bool) []string {
+	if test {
+		return []string{
+			"00100",
+			"11110",
+			"10110",
+			"10111",
+			"10101",
+			"01111",
+			"00111",
+			"11100",
+			"10000",
+			"11001",
+			"00010",
+			"01010",
+		}
+	}
+
+	b, err := os.ReadFile("input.txt")
+	if err != nil {
+		fmt.Println(err.Error())
+		os.Exit(1)
+	}
+	split := bytes.Split(b, []byte("\n"))
+	out := make([]string, len(split))
+	for i, e := range split {
+		out[i] = string(e)
+	}
+	return out
+}
+
+func powerConsumption(input []string) int64 {
+	gamma := strings.Builder{}
+	epsilon := strings.Builder{}
+	for j := 0; j < len(input[0]); j++ {
+		one := 0
+		zero := 0
+		for i := 0; i < len(input); i++ {
+			if string(input[i][j]) == "1" {
+				one++
+			} else {
+				zero++
+			}
+		}
+		if one > zero {
+			gamma.WriteString("1")
+			epsilon.WriteString("0")
+		} else {
+			gamma.WriteString("0")
+			epsilon.WriteString("1")
+		}
+	}
+	gammaD, err := strconv.ParseInt(gamma.String(), 2, 64)
+	if err != nil {
+		fmt.Println(err.Error())
+		os.Exit(1)
+	}
+	epsilonD, err := strconv.ParseInt(epsilon.String(), 2, 64)
+	if err != nil {
+		fmt.Println(err.Error())
+		os.Exit(1)
+	}
+	return gammaD * epsilonD
+}
+
+func lifeSupportRating(oxygen bool, arr []string, current int) []string {
+	arrLenHalf := float64(len(arr)) / 2
+	ones := make([]int, 0)
+	for i := range arr[0] {
+		ones = append(ones, 0)
+		for _, j := range arr {
+			if j[i] == byte('1') {
+				ones[i]++
+			}
+		}
+	}
+	pick := float64(ones[current])
+	common := partTwoByteMap[pick >= arrLenHalf][oxygen]
+	out := make([]string, 0)
+	for _, e := range arr {
+		if e[current] == common {
+			out = append(out, e)
+		}
+	}
+	return out
+}
+
+func main() {
+	flag.Parse()
+
+	commands := getReport(*flagTest)
+	start := time.Now()
+	p1 := powerConsumption(commands)
+	p1time := time.Since(start)
+	start = time.Now()
+
+	for i := range commands[0] {
+		commands = lifeSupportRating(true, commands, i)
+		if len(commands) == 1 {
+			break
+		}
+	}
+	OxygenGeneratorRating, err := strconv.ParseInt(commands[0], 2, 64)
+	if err != nil {
+		fmt.Println(err.Error())
+		os.Exit(1)
+	}
+
+	commands = getReport(*flagTest)
+	for i := range commands[0] {
+		commands = lifeSupportRating(false, commands, i)
+		if len(commands) == 1 {
+			break
+		}
+	}
+	CO2ScrubberRating, err := strconv.ParseInt(commands[0], 2, 64)
+	if err != nil {
+		fmt.Println(err.Error())
+		os.Exit(1)
+	}
+
+	p2 := OxygenGeneratorRating * CO2ScrubberRating
+	p2time := time.Since(start)
+
+	fmt.Printf("Part 1:\t\t%d\t(%s)\nPart 2:\t\t%d\t(%s)\n", p1, p1time, p2, p2time)
+	fmt.Printf("Total Time:\t%s\n", p1time+p2time)
+}
